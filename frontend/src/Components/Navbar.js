ca5781844fb8ec122521859fa949299bc1dc0bfc
Chethan Pai <mcpai217@gmail.com>
2023-08-12 15:57:52 +0530

    frontend

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">PRAGATI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/" className="nav-link active">Home</Nav.Link>
+            <Nav.Link as={Link} to="/admin" className="nav-link">Admin</Nav.Link>
+            <Nav.Link as={Link} to="/books" className="nav-link">Books</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="nav navbar-nav navbar-right">
+            <Nav.Link as={Link} to="/signup" className="nav-link btn btn-primary">
+              <span className="glyphicon glyphicon-user"></span> Sign Up
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login" className="nav-link btn btn-primary">
+              <span className="glyphicon glyphicon-log-in"></span> Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
---
+++ frontend/src/Components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <div className="container">
+        <p>&copy; 2023 Your Library. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
