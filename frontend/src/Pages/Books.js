dc77b6f28f2036c96dec03b160a3117c8e6eba3a
Anudeep K K <anudeep2003@gmail.com>
2023-08-14 20:58:09 +0530

    CSS for books

--- frontend/src/Pages/Books.js
+++ frontend/src/Pages/Books.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Book1 from "../Images/Book1.png"
 import Book2 from "../Images/Book2.png"
 import Book3 from "../Images/Book3.png"
@@ -85,67 +85,83 @@ const Books = () => {
 
     return (
         <div className="container mt-5">
-            <h1 className="mb-4">Available Books</h1>
-            <div className="row mb-3">
-                <div className="col-md-6">
-                    <div className="input-group">
-                        <input
-                            id="Search-bar"
-                            type="text"
-                            className="form-control"
-                            placeholder="Search by title or author"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button" onClick={handleSearch}>
-                                Search
-                            </button>
-                        </div>
-                    </div>
+            <div className="border border-5 p-4 bg-warning-subtle">
+                <div className="mb-4 text-center border border-5 border-black">
+                    <h1>BOOK SHELF</h1>
                 </div>
-            </div>
-            <div className="row mb-3">
-                <div className="col-md-6">
-                    <div className="mb-3">
-                        <label htmlFor="genreFilter" className="form-label">
-                            Filter by Genre:
-                        </label>
-                        <select
-                            id="genreFilter"
-                            className="form-select"
-                            value={selectedGenre}
-                            onChange={(e) => setSelectedGenre(e.target.value)}
-                        >
-                            <option value="None">None</option>
-                            <option vaue="Fiction">Fiction</option>
-                            {/* Add more genre options here */}
-                        </select>
-                    </div>
-                </div>
-            </div>
-            {showResults && (
-    <div className="d-flex justify-content-between align-items-center mb-3">
-        <h2 className="mb-0">Results</h2>
-        <button className="btn btn-outline-secondary" onClick={handleBack}>
-            Back
-        </button>
-    </div>
-)}
-            <div className="row">
-                {(showResults ? filteredBooks : filteredGenreBooks).map((book) => (
-                    <div key={book._id} className="col-md-3 mb-4">
-                        <div className="card">
-                            <img src={book.coverImage} alt={`Cover of ${book.title}`} className="card-img-top" />
-                            <div className="card-body">
-                                <h5 className="card-title">{book.title}</h5>
-                                <p className="card-text">Author: {book.author}</p>
-                                <p className="card-text">Genre: {book.genre}</p>
-                                <p className="card-text">Publication Date: {book.publicationDate}</p>
+                <div className="border border-4 p-3 bg-primary-subtle">
+                    <h2 className="mb-4 text-center">Find Available Books</h2>
+                    <div className="row mb-3 justify-content-center">
+                        <div className="col-md-6  d-flex justify-content-center align-items-center ">
+                            <div className="input-group">
+                                <input
+                                    id="Search-bar"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Search by title or author"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
+                                <div className="col-md-2 d-flex justify-content-center align-items-center">
+                                    <button className="btn btn-outline-secondary bg-dark text-info" type="button" onClick={handleSearch}>
+                                        Search
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                ))}
+                    <div className="row mb-3 justify-content-center">
+                        <div className="col-md-6">
+                            <div className="mb-3">
+                                <label htmlFor="genreFilter" className="form-label">
+                                    Filter by Genre:
+                                </label>
+                                <select
+                                    id="genreFilter"
+                                    className="form-select"
+                                    value={selectedGenre}
+                                    onChange={(e) => setSelectedGenre(e.target.value)}
+                                >
+                                    <option value="None">None</option>
+                                    <option vaue="Fiction">Fiction</option>
+                                    <option vaue="Dystopian">Dystopian</option>
+                                    <option vaue="Fantasy">Fantasy</option>
+                                    <option vaue="Romance">Romance</option>
+                                    {/* Add more genre options here on need*/}
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-5 mt-3 p-5 bg-primary-subtle">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12">
+                            {showResults && (
+                                <div className="d-flex justify-content-center align-items-center mt-3 mb-4">
+                                    <h6 className="mb-0 me-3 border border-3 border-info text-info p-2 rounded bg-dark">Results</h6>
+                                    <button className="btn btn-outline-secondary ml-3 text-info border border-3 border-info bg-black" onClick={handleBack}>
+                                        Back
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className="row">
+                        {(showResults ? filteredBooks : filteredGenreBooks).map((book) => (
+                            <div key={book._id} className="col-md-3 mb-4">
+                                <div className="card">
+                                    <img src={book.coverImage} alt={`Cover of ${book.title}`} className="card-img-top" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{book.title}</h5>
+                                        <p className="card-text">Author: {book.author}</p>
+                                        <p className="card-text">Genre: {book.genre}</p>
+                                        <p className="card-text">Publication Date: {book.publicationDate}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
