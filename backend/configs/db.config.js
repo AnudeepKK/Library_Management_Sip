ab17041fba7be58b61d3e4f90286659f299c9760
Chethan Pai <mcpai217@gmail.com>
2023-08-14 22:06:15 +0530

    update

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME : "idea_app_demo",
-    DB_URL : "mongodb+srv://chethancs21:collegescem@cluster0.x1emexn.mongodb.net/"
+    DB_URL : ""                   //mongodb url
 }
