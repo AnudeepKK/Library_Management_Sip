edcde9c5cdc1a395ec4e1bfef962861244811976
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-08-16 22:41:39 +0530

    Feat:about in dropdown

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -14,11 +14,11 @@ const CustomNavbar = () => {
             <Nav.Link as={Link} to="/" className="nav-link active">Home</Nav.Link>
             <Nav.Link as={Link} to="/admin" className="nav-link">Admin</Nav.Link>
             <Nav.Link as={Link} to="/books" className="nav-link">Books</Nav.Link>
-            <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link>
+            {/* <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link> */}
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="/about">About</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
