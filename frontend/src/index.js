811f2b902b1cfef4648e9bbfdd990ae746d09014
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-09-05 00:03:28 +0530

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Imgbg from './Images/lib.jpg';
+import Imgbg from './Images/Libcover.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className='bg-warning'>
+  <div style={{ backgroundImage: `url(${Imgbg})`, backgroundSize: 'cover' }}>
     <App />
   </div>
 );
